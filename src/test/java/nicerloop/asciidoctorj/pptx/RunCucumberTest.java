635f236b141808873ce5d006e69b8c5e4887e98c
Loïc PÉRON <loic.peron@gmail.com>
2024-03-21 17:46:42 +0100

    build: add serenity, requires Java 11

--- src/test/java/nicerloop/asciidoctorj/pptx/RunCucumberTest.java
+++ src/test/java/nicerloop/asciidoctorj/pptx/RunCucumberTest.java
@@ -2,6 +2,7 @@ package nicerloop.asciidoctorj.pptx;
 
 import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -11,6 +12,7 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "nicerloop.asciidoctorj.pptx")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
 public class RunCucumberTest {
 
 }
