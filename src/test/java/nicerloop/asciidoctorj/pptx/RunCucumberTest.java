41f3b47144740af737b504e761b6e0fbd8348ff7
Loïc PÉRON <loic.peron@gmail.com>
2024-07-10 16:39:48 +0200

    tests: use JUnit Suite @SelectFile instead of FEATURES parameter

--- src/test/java/nicerloop/asciidoctorj/pptx/RunCucumberTest.java
+++ src/test/java/nicerloop/asciidoctorj/pptx/RunCucumberTest.java
@@ -1,16 +1,16 @@
 package nicerloop.asciidoctorj.pptx;
 
-import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectFile;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
+@SelectFile("src/test/resources/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "nicerloop.asciidoctorj.pptx")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
 public class RunCucumberTest {
